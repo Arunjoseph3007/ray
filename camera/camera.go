3c3c224ed929150e325a4584b42a4b750bb35221
Arunjoseph3007 <arunjoseph3007@gmail.com>
2023-09-13 20:21:34 +0530

    Consolidate camera to own struct

---
+++ camera/camera.go
@@ -0,0 +1,114 @@
+package camera
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"ray-tracing/hit"
+	"ray-tracing/ray"
+	"ray-tracing/utils"
+	"ray-tracing/vec"
+)
+
+type Camera struct {
+	center        vec.Point
+	pixel00_loc   vec.Point
+	pixel_delta_u vec.Vec3
+	pixel_delta_v vec.Vec3
+	AspectRatio   float64
+	Width         int
+	Height        int
+}
+
+var WHITE = vec.New(1, 1, 1)
+var SKY_BLUE = vec.New(0.5, 0.7, 1)
+
+func (c *Camera) ray_color(r ray.Ray, h hit.HitList) vec.Color {
+	hit_data := hit.HitData{}
+
+	if h.Hit(r, utils.NewInterval(0, math.Inf(1)), &hit_data) {
+		return *vec.MulScalar(
+			*vec.Add(
+				hit_data.Normal,
+				*vec.New(1, 1, 1),
+			),
+			0.5,
+		)
+	}
+
+	dir := vec.UnitVec(r.Direction)
+	a := 0.5 * (dir.Y() + 1)
+
+	return *vec.Add(
+		*vec.MulScalar(*WHITE, 1-a),
+		*vec.MulScalar(*SKY_BLUE, a),
+	)
+}
+
+func (c *Camera) Initialize() {
+	c.AspectRatio = 16.0 / 9.0
+	c.Width = 200
+	c.Height = int(float64(c.Width) / c.AspectRatio)
+
+	focal_length := 1.0
+	viewport_height := 2.0
+	viewport_width := viewport_height * float64(c.Width) / float64(c.Height)
+	c.center = *vec.New(0, 0, 0)
+
+	viewport_u := vec.New(viewport_width, 0, 0)
+	viewport_v := vec.New(0, -viewport_height, 0)
+
+	c.pixel_delta_u = *vec.DivScalar(*viewport_u, float64(c.Width))
+	c.pixel_delta_v = *vec.DivScalar(*viewport_v, float64(c.Height))
+
+	viewport_up_left := vec.Sub(
+		c.center,
+		*vec.Add(
+			*vec.New(0, 0, focal_length),
+			*vec.DivScalar(*viewport_u, 2),
+			*vec.DivScalar(*viewport_v, 2),
+		),
+	)
+	c.pixel00_loc = *vec.Add(
+		*viewport_up_left,
+		*vec.DivScalar(c.pixel_delta_u, 2),
+		*vec.DivScalar(c.pixel_delta_v, 2),
+	)
+}
+
+func (c *Camera) Render(world hit.HitList) {
+	out := fmt.Sprintf("P3\n%d %d\n255\n", c.Width, c.Height)
+
+	for j := 0; j < c.Height; j++ {
+		fmt.Printf("\rRemaining: %f /", float64(j)/float64(c.Height)*100)
+		for i := 0; i < c.Width; i++ {
+			pixel_center := *vec.Add(
+				c.pixel00_loc,
+				*vec.MulScalar(c.pixel_delta_u, float64(i)),
+				*vec.MulScalar(c.pixel_delta_v, float64(j)),
+			)
+			ray_direction := vec.Sub(
+				pixel_center,
+				c.center,
+			)
+			r := ray.New(c.center, *ray_direction)
+
+			pixel_color := c.ray_color(*r, world)
+			out += pixel_color.ToClrStr()
+		}
+	}
+
+	file, err := os.Create("image.ppm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err2 := file.WriteString(out)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("Done!")
+}
